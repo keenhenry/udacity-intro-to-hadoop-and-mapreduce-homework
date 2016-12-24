776a70b122b05bd8ddae16ff5b6f41d28cb0ce6d
henry <keenhenry1109@gmail.com>
2016-12-24 17:22:34 -0500

    reducer implementation also suited for use by both the combiner and reducer

--- stores/reducer.py
+++ stores/reducer.py
@@ -4,13 +4,12 @@ import sys
 from collections import defaultdict
 
 sales_per_weekday = defaultdict(float)
-num_sales_per_weekday = defaultdict(int)
 
 # Loop around the data
 # It will be in the format key\tval
 # Where key is the weekday name, val is the sale amount
 #
-# Output the average sales for each weekday
+# Output the total sales for each weekday
 
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
@@ -20,8 +19,7 @@ for line in sys.stdin:
 
     weekday, cost = data_mapped
     sales_per_weekday[weekday] += float(cost)
-    num_sales_per_weekday[weekday] += 1
 
 # print out the results
 for day in sales_per_weekday:
-    print 'Averate sale for {day}: {sale}'.format(day=day, sale=sales_per_weekday[day]/float(num_sales_per_weekday[day]))
+    print '{day}\t{sale}'.format(day=day, sale=sales_per_weekday[day])
